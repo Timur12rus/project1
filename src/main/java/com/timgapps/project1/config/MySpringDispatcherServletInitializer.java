5e13887ad757af0dd195ef72ee347e0ca19e4268
Timur <tim.gordeev2015@yandex.ru>
2023-05-12 22:55:23 +0300

    Start Project1

---
+++ src/main/java/com/timgapps/project1/config/MySpringDispatcherServletInitializer.java
@@ -0,0 +1,17 @@
+package com.timgapps.project1.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ src/main/java/com/timgapps/project1/models/Person.java
@@ -0,0 +1,4 @@
+package com.timgapps.project1.models;
+
+public class Person {
+}
