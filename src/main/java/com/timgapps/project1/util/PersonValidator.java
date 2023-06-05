e4d21c93f748b6d04d730337620439745ebf443f
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-06-05 16:52:48 +0300

    Add PersonValidator

--- src/main/java/com/timgapps/project1/util/PersonValidator.java
+++ src/main/java/com/timgapps/project1/util/PersonValidator.java
@@ -7,25 +7,25 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 // т.к. ФИО уникальное, нужен валидатор
 
-//@Component
-//public class PersonValidator implements Validator {
-//
-//    private final PersonDAO personDAO;
-//
-//    public PersonValidator(PersonDAO personDAO) {
-//        this.personDAO = personDAO;
-//    }
-//
-//    @Override
-//    public boolean supports(Class<?> aClass) {
-//        return Person.class.equals(aClass);
-//    }
-//
-//    @Override
-//    public void validate(Object o, Errors errors) {
-//        Person person = (Person) o;
-//
-////        if (personDAO.getPersonByFullName(person.getFullName()).isPresent())
-////            errors.rejectValue("fullName", "", "Человек с таким именем уже существует!");
-////    }
-//}
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+//        if (personDAO.getPersonByFullName(person.getFullName()).isPresent())
+//            errors.rejectValue("fullName", "", "Человек с таким именем уже существует!");
+    }
+}
