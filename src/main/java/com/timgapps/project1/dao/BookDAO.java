bf56bce06a5612401baa418165115dcfe5a95881
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-06-10 14:39:37 +0300

    Finish Project1

--- src/main/java/com/timgapps/project1/dao/BookDAO.java
+++ src/main/java/com/timgapps/project1/dao/BookDAO.java
@@ -55,7 +55,7 @@ public class BookDAO {
                 "WHERE Book.id = ?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
 
-    // освобождает книгу человеку(этот метод вызывается, когда человек возвращает книгу в библиотекуz)
+    // освобождает книгу человеку(этот метод вызывается, когда человек возвращает книгу в библиотеку)
     public void release(int id) {
         jdbcTemplate.update("UPDATE Book SET person_id=NULL WHERE id=?", id);
     }
